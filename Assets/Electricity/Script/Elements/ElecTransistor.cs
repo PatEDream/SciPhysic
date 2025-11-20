35888273ee0b09b8378f0a3321295f4cc9ab7b4a
Patrick <pk.marchal@gmail.com>
2025-11-20 17:50:18 +0100

    Deuxième essai transistor
    # Conflicts:
    #	Assets/Electricity/Script/Elements/ElecBattery.cs

--- Assets/Electricity/Script/Elements/ElecTransistor.cs
+++ Assets/Electricity/Script/Elements/ElecTransistor.cs
@@ -8,10 +8,31 @@ public class ElecTransistor : ElecSwitch
     public float potentialThreshold = 0.7f;
 
 
-    override public void ComputeIntensity()
+    //override public void ComputeIntensity()
+    //{
+    //    isOpen = baseNode.potential < potentialThreshold;
+    //    base.ComputeIntensity();
+    //}
+
+    //override public void IntensityToDeltaPotential()
+    //{
+    //    sta.deltaPotential -= intensity * ElecManager.intensityToPotentialEffect;
+    //    end.deltaPotential += intensity * ElecManager.intensityToPotentialEffect;
+    //}
+
+    override public void ComputeDeltaPotential()
     {
-        isOpen = baseNode.potential < potentialThreshold;
-        base.ComputeIntensity();
+        double VoltageBase = baseNode.potential - end.potential;
+        double VoltageCut = VoltageBase < potentialThreshold ? VoltageBase : potentialThreshold;
+        if (VoltageBase > 0.0 && VoltageCut > 0.0)
+        {
+            baseNode.deltaPotential -= VoltageCut * ElecManager.conductanceWire;
+            end.deltaPotential -= VoltageCut * ElecManager.conductanceWire;
+        }
+
+        isOpen = (VoltageBase < potentialThreshold);
+
+        base.ComputeDeltaPotential();
     }
 
 
